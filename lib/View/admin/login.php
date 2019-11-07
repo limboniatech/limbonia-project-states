0f3d9530cf8c048ee7d71f4385b025f143272f3e
Lonnie Blansett <lonnie@limbonia.tech>
2019-11-07 10:34:40 -0600

    Minor tweaks and some name fixing

--- lib/View/admin/login.php
+++ lib/View/admin/login.php
@@ -4,24 +4,24 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Limbonia Password Check</title>
-  <style>
 <?php
 $sDefaultCSSFile = $app->getDir('root') . '/share/login.css';
 
 if (is_readable($sDefaultCSSFile))
 {
+  echo "  <style>\n";
   echo file_get_contents($sDefaultCSSFile);
+  echo "  </style>\n";
 }
 else
 {
-  echo "  <link rel=\"stylesheet\" type=\"text/css\" href=\"<?= $this->domain->uri . '/' . $this->getDir('share') ?>/login.css\" />";
+  echo '  <link rel="stylesheet" type="text/css" href="' . $app->domain->uri . '/' . $app->getDir('share') . '/login.css" />';
 }
 ?>
-  </style>
 </head>
 <body onLoad="document.passCheck.email.focus();">
-<form action="" method="post" name="passCheck">
-    <?= (isset($failure) ? $failure : '') ?>
+  <form action="" method="post" name="passCheck">
+<?= (isset($failure) ? $failure : '') ?>
     <div class="field"><span class="name">Email:</span><span class="value"><input type="text" name="email"></span></div>
     <div class="field"><span class="name">Password:</span><span class="value"><input type="password" name="password"></span></div>
     <div class="field"><span class="name"></span><span class="value"><input type="submit" name="submit" value="Authorization"></span></div>
